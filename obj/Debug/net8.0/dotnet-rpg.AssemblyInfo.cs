f31d5e52851f869bca9bcb1f958ba5ce950b363a
JP Guiang <jpguiang15@gmail.com>
2024-08-23 16:44:44 +1200

    chore: remove duplicate sln file

--- obj/Debug/net8.0/dotnet-rpg.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-rpg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-rpg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec028b37707ce162bd3366604e931b64a2c9f705")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecafc593e04ead248128dba02cecf3b395d4eb49")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-rpg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-rpg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
